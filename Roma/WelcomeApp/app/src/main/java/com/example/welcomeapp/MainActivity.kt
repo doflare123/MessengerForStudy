c6a6cb6956ac9d083c571a122c4ad5972f916773
doflare123 <144382600+doflare123@users.noreply.github.com>
2024-12-08 19:51:41 +0200

    minifix

--- Roma/WelcomeApp/app/src/main/java/com/example/welcomeapp/MainActivity.kt
+++ Roma/WelcomeApp/app/src/main/java/com/example/welcomeapp/MainActivity.kt
@@ -11,12 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val str = "Прикол2"
+        val str2 = "Прикол"
 
         // Найти элементы интерфейса
         val startButton = findViewById<Button>(R.id.start_button)
         val welcomeText = findViewById<TextView>(R.id.welcome_text)
         val welcomeImage = findViewById<ImageView>(R.id.welcome_image)
         val groupText = findViewById<TextView>(R.id.group_text)
+        val textPrikol = findViewById<TextView>(R.id.main_textview)
 
         // Установить видимость текста и изображения невидимой
         welcomeText.visibility = View.GONE
@@ -28,10 +31,12 @@ class MainActivity : AppCompatActivity() {
                 welcomeText.visibility = View.GONE
                 welcomeImage.visibility = View.GONE
                 groupText.visibility = View.GONE
+                textPrikol.text = str2
             } else {
                 welcomeText.visibility = View.VISIBLE
                 welcomeImage.visibility = View.VISIBLE
                 groupText.visibility = View.VISIBLE
+                textPrikol.text = str
             }
         }
 
