18af512810d2593c4c45dc54e3cb28961bd5d38a
Vitamin D <85170824+Vitamins-D@users.noreply.github.com>
2025-01-17 16:42:22 +0200

    Fix

--- Messenger/app/(tabs)/Screens/LoginMenu.jsx
+++ Messenger/app/(tabs)/Screens/LoginMenu.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useCallback, useState } from 'react';
-import { Text, TextInput, TouchableOpacity, StyleSheet, Alert, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, StyleSheet, Alert, View, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform  } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Feather.js';
 //import { useWebSocket } from '@/WebSoket/WSConnection';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -9,6 +10,7 @@ import styles from '../../Styles/Styles.js';
 export default function LoginScreen({ navigation }) {
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+    const [isSecure, setSecure] = useState(true);
 
     const handleEmailChange = (text) => {
         setEmail(text);
@@ -18,48 +20,66 @@ export default function LoginScreen({ navigation }) {
         setPassword(text);
     };
     
+    const handleSecureChange = () => {
+        setSecure(!isSecure);
+    };
 
     return (
-        <SafeAreaView style={[styles.lightBg, {paddingLeft: 60, paddingRight: 60}]}>
-            <View style={styles.headerView}>
-                <Text style={styles.headerLight}>Вход</Text>
-            </View>
-            <View style={{flex: 1.5}}>
-                <TextInput
-                    style={styles.lightBorderInput}
-                    value={email}
-                    placeholder="Введите почту"
-                    keyboardType="email-address"
-                    onChangeText={handleEmailChange}
-                    contentInset={{ top: 0, left: 10, bottom: 0, right: 10 }}
-                />
-                <TextInput
-                    style={styles.lightBorderInput}
-                    value={password}
-                    secureTextEntry={true}
-                    onChangeText={handlePswdChange}
-                />
-                <TouchableOpacity>
-                    <Text style={[styles.lightTextBg, {fontSize: 15, marginTop: 10, textAlign: 'right'}]}>Забыли пароль?</Text>
-                </TouchableOpacity>
-                <View style={styles.viewEnd}>
-                    <TouchableOpacity style={[lstyles.enterBtn, styles.lightBtn]}>
-                        <Text style={[styles.lightText, {fontSize: 30}]}>Войти</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <View style={{flex: 1}}>
-                <View style={styles.companyLineView}>
-                    <View style={[styles.horizontalLine, {marginTop: 60}]}></View>
-                    <TouchableOpacity>
-                        <Text style={[styles.lightTextBg, {fontSize: 18}]}>Зарегистрироваться</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.viewEnd}>
-                    <Text style={styles.companyLight}>©Necrodwarf</Text>
-                </View>
-            </View>
-        </SafeAreaView>
+        <KeyboardAvoidingView
+            style={{flex: 1}}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={-250} // смещение клавиатуры
+        >
+            <TouchableWithoutFeedback
+                onPress={Keyboard.dismiss} // скрытие клавиатуры при нажатии вне TextInput
+            >
+                <SafeAreaView style={[styles.lightBg, {paddingLeft: 60, paddingRight: 60}]}>
+                    <View style={styles.headerView}>
+                        <Text style={styles.headerLight}>Вход</Text>
+                    </View>
+                    <View style={{flex: 1.5}}>
+                        <TextInput
+                            style={styles.lightBorderInput}
+                            value={email}
+                            placeholder="Введите почту"
+                            keyboardType="email-address"
+                            onChangeText={handleEmailChange}
+                            contentInset={{ top: 0, left: 10, bottom: 0, right: 10 }}
+                        />
+                        <View style={{position: "relative", flex: 1}}>
+                            <TextInput
+                                style={[styles.lightBorderInput]}
+                                value={password}
+                                secureTextEntry={true}
+                                onChangeText={handlePswdChange}
+                            />
+                            <TouchableOpacity onPress={handleSecureChange}>
+                                <Icon name={isSecure ? 'eye-off' : 'eye'} size={24} color="#00000" style={{position: "absolute", right: 20, top: -37}}/>
+                            </TouchableOpacity>
+                        </View>
+                        <TouchableOpacity>
+                            <Text style={[styles.lightTextBg, {fontSize: 15, marginTop: 0, textAlign: 'right'}]}>Забыли пароль?</Text>
+                        </TouchableOpacity>
+                        <View style={styles.viewEnd}>
+                            <TouchableOpacity style={[lstyles.enterBtn, styles.lightBtn]}>
+                                <Text style={[styles.lightText, {fontSize: 30}]}>Войти</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <View style={styles.companyLineView}>
+                            <View style={[styles.horizontalLine, {marginTop: 60}]}></View>
+                            <TouchableOpacity>
+                                <Text style={[styles.lightTextBg, {fontSize: 18}]}>Зарегистрироваться</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.viewEnd}>
+                            <Text style={styles.companyLight}>©Necrodwarf</Text>
+                        </View>
+                    </View>
+                </SafeAreaView>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }
 
