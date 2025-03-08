a043a8c04b839adf905b251309d88430e722c11a
doflare123 <144382600+doflare123@users.noreply.github.com>
2025-03-08 22:04:54 +0200

    minifix

--- verification_api/database.js
+++ verification_api/database.js
@@ -2,9 +2,16 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config({ path: "./.env" });
 
-const connection = new Sequelize(process.env.BD_post_namebd, process.env.BD_post_username, process.env.BD_post_pswd , {
-    host: process.env.BD_post_adress,
-    dialect: 'postgres'
-});
+const connection = new Sequelize(process.env.BD_post_uri, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, // Для использования SSL
+        rejectUnauthorized: false, // Если ты уверен в безопасности
+      },
+    },
+    logging: false,
+  });
+  
 
 module.exports = connection;
