739e785a7645fdb92dc0dcf48283ae1e1e47dfa1
doflare123 <144382600+doflare123@users.noreply.github.com>
2025-04-10 23:14:25 +0200

    fix http methods

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
 
-router.post('/ChangeAvatar', accountController.ChangeAvatar);
-router.post('/ChangeUserName', accountController.ChangeUserName);
-router.post('/PswdChng', accountController.PswdChng);
-router.post('/DeleteAccount', accountController.DeleteAccount);
+router.patch('/ChangeAvatar', accountController.ChangeAvatar);
+router.patch('/ChangeUserName', accountController.ChangeUserName);
+router.patch('/PswdChng', accountController.PswdChng);
+router.delete('/DeleteAccount', accountController.DeleteAccount);
 router.post('/RefreshToken', accountController.RefreshToken);
 
 module.exports = router;
